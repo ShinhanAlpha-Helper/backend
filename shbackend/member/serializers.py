7e454a447d07c79f85930aa9edf18a86eaa26959
kjungw1025 <kjungw1025@naver.com>
2023-02-19 21:38:30 +0900

    feat: Create serializers.py

---
+++ shbackend/member/serializers.py
@@ -0,0 +1,22 @@
+from rest_framework import serializers
+from .models import Member
+from django.contrib.auth.hashers import make_password
+
+class MemberRegisterSerializer (serializers.ModelSerializer):
+    def validate_password (self, value):
+        if len(value) < 8:
+            raise serializers.ValidationError('Too short password')
+        return make_password(value)
+
+    class Meta:
+        model = Member
+        fields = ('id', 'username', 'useremail', 'password')
+
+        extra_kwargs = {
+            'id': {
+                'read_only': True,
+            },
+            'password': {
+                'write_only': True
+            }
+        }
