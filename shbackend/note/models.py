7384350686222fac4312938c7351adb5b1ee2a98
kjungw1025 <kjungw1025@naver.com>
2023-02-21 13:27:33 +0900

    feat: Update models.py add count column

--- shbackend/note/models.py
+++ shbackend/note/models.py
@@ -4,9 +4,10 @@ from django.db import models
 
 class Note (models.Model):
     member = models.ForeignKey('member.Member', on_delete=models.CASCADE, verbose_name='사용자')
-    title = models.CharField(max_length=128, unique=True, verbose_name="용어")
+    title = models.CharField(max_length=128, verbose_name="용어")
     content = models.TextField(verbose_name="설명")
     tstamp = models.DateTimeField(auto_now_add=True, verbose_name='등록일시')
+    count = models.IntegerField(default=1)
 
     def __str__(self):
         return self.title
