8c3370b54a7acc381e51d018fb28146a62b8091f
kjungw1025 <kjungw1025@naver.com>
2023-02-21 13:29:08 +0900

    feat: Update urls.py add today, weekly link

--- shbackend/note/urls.py
+++ shbackend/note/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path("", views.NoteView.as_view()),
     path("/detail", views.NoteDetailView.as_view()),
+    path("/rank/today", views.NoteTodayRankView.as_view()),
+    path("/rank/weekly", views.NoteWeeklyRankView.as_view()),
 ]
