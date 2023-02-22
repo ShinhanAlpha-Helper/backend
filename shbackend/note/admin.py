5ec07fbcbad7887a6303290f0d0a8754f4aa151c
kjungw1025 <kjungw1025@naver.com>
2023-02-22 17:11:04 +0900

    feat: Update admin.py

--- shbackend/note/admin.py
+++ shbackend/note/admin.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
-from .models import Note
+from .models import Note, Bookmark
 
 # Register your models here.
 
 @admin.register(Note)
 class NoteAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Bookmark)
+class BookmarkAdmin(admin.ModelAdmin):
     pass
