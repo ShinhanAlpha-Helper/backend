04da002d6f761bc7977b8ab05a8d5aaa11a4f59e
kjungw1025 <kjungw1025@naver.com>
2023-02-22 17:13:07 +0900

    feat: Update urls.py

--- shbackend/note/urls.py
+++ shbackend/note/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path("/detail", views.NoteDetailView.as_view()),
     path("/rank/today", views.NoteTodayRankView.as_view()),
     path("/rank/weekly", views.NoteWeeklyRankView.as_view()),
+    path("/bookmark", views.BookmarkCreateView.as_view()),
+    path("/bookmark/view", views.BookmarkView.as_view()),
 ]
